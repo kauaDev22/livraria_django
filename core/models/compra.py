5058496868a37eb8ae9ab1ea03ca5000d167f3b1
kauaDev22 <eukauapro@gmail.com>
2024-12-18 15:03:19 -0300

    Gravando o preço do livro no item da compra

--- core/models/compra.py
+++ core/models/compra.py
@@ -13,6 +13,7 @@ class Compra(models.Model):
     
     usuario = models.ForeignKey(User, on_delete=models.PROTECT, related_name="compras")
     status = models.IntegerField(choices=StatusCompra.choices, default=StatusCompra.CARRINHO)
+    preco = models.DecimalField(max_digits=10, decimal_places=2, default=0)
 
     def __str__(self):
         return f"Compra {self.id} - {self.usuario}"
@@ -27,10 +28,6 @@ class ItensCompra(models.Model):
     
     @property
     def total(self):
-        # total = 0
-        # for item in self.itens.all():
-        #     total += item.livro.preco * item.quantidade
-        # return total
-        return sum(item.livro.preco * item.quantidade for item in self.itens.all())
+        return sum(item.preco * item.quantidade for item in self.itens.all())
 
 
